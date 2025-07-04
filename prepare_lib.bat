44a91e54822fb35747fda3bcce0e8ef17d09e781
Bulka <sihidov.timofii@gmail.com>
2025-07-04 20:40:15 +0300

    added script prepare_lib.bat

---
+++ prepare_lib.bat
@@ -0,0 +1,11 @@
+rmdir /s /q lib
+mkdir lib
+mkdir lib\bcppul
+mkdir lib\bcppul\include
+mkdir lib\bcppul\lib
+copy build_debug\lib\Debug\bcppul_static.lib lib\bcppul\lib\bcppul_static_debug.lib
+copy build_debug\bin\Debug\bcppul_shared.dll lib\bcppul\lib\bcppul_shared_debug.dll
+copy build_release\lib\Release\bcppul_static.lib lib\bcppul\lib\bcppul_static_release.lib
+copy build_release\bin\Release\bcppul_shared.dll lib\bcppul\lib\bcppul_shared_release.dll
+xcopy build_release\bcppul\* lib\bcppul\include\bcppul\ /E
+xcopy include\bcppul\* lib\bcppul\include\bcppul\ /E
